af4f74b3d26b4917ad1f585852f195e39d299d87
hyunsukimsokcho <hskimsokcho@gmail.com>
2020-12-01 17:56:35 +0900

    [Benchmark] fix build script in optipng

--- benchmark/optipng/0.5.3/build.sh
+++ benchmark/optipng/0.5.3/build.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+MAKE_PARAMS="-j -C src/ -f scripts/gcc.mak"
+
 if [[ $1 == "sparrow" ]]; then
   $SMAKE_BIN --init
-  $SMAKE_BIN -j -C src/ -f scripts/gcc.mak
+  $SMAKE_BIN $MAKE_PARAMS
   mv sparrow/src/optipng/*.i $SMAKE_OUT
 elif [[ $1 == "infer" ]]; then
-  $INFER_BIN capture -- make -j
+  $INFER_BIN capture -- make $MAKE_PARAMS
   mv infer-out $OUT
 else
   echo "Unknown build target"

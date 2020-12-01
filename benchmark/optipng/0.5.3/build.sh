69828f6acb4877d28499501171ddf791a5a98dd8
hyunsukimsokcho <hskimsokcho@gmail.com>
2020-12-01 17:48:28 +0900

    [Benchmark] add build script for optipng/0.5.3

--- benchmark/optipng/0.5.3/build.sh
+++ benchmark/optipng/0.5.3/build.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
 
+if [[ $1 == "sparrow" ]]; then
+  $SMAKE_BIN --init
+  $SMAKE_BIN -j -C src/ -f scripts/gcc.mak
+  mv sparrow/src/optipng/*.i $SMAKE_OUT
+elif [[ $1 == "infer" ]]; then
+  $INFER_BIN capture -- make -j
+  mv infer-out $OUT
+else
+  echo "Unknown build target"
+  exit 1
+fi

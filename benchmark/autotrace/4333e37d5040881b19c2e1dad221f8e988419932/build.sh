f63367b264c0cea66bf8f1020304eb13b71e021a
Kihong Heo <kihong.heo@kaist.ac.kr>
2020-12-01 15:14:16 +0900

    [Benchmark] update docker structure

--- benchmark/autotrace/4333e37d5040881b19c2e1dad221f8e988419932/build.sh
+++ benchmark/autotrace/4333e37d5040881b19c2e1dad221f8e988419932/build.sh
@@ -2,3 +2,15 @@
 
 ./autogen.sh
 ./configure
+
+if [[ $1 == "sparrow" ]]; then
+  $SMAKE_BIN --init
+  $SMAKE_BIN -j
+  mv sparrow/src/*.i $SMAKE_OUT
+elif [[ $1 == "infer" ]]; then
+  $INFER_BIN capture -- make -j
+  mv infer-out $OUT
+else
+  echo "Unknown build target"
+  exit 1
+fi

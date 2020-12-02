e06d87d4467071bb92198be0daadd36929d81dc4
Kihong Heo <kihong.heo@kaist.ac.kr>
2020-12-03 02:37:01 +0900

    [Benchmark] add grep

---
+++ benchmark/grep/2.19/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+./configure
+
+if [[ $1 == "sparrow" ]]; then
+  $SMAKE_BIN --init
+  $SMAKE_BIN -j
+  cp sparrow/src/grep/*.i $SMAKE_OUT
+elif [[ $1 == "infer" ]]; then
+  $INFER_BIN capture -- make -j
+  mv infer-out $OUT
+else
+  echo "Unknown build target"
+  exit 1
+fi

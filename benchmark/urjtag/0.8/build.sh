1cb33865f2e92ec034a3496c46d1f71eb1a071e1
hyunsukimsokcho <hskimsokcho@gmail.com>
2020-12-02 14:22:00 +0900

    [Benchmark] add urjtag

---
+++ benchmark/urjtag/0.8/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+./configure
+
+MAKE_PARAMS="-j"
+
+if [[ $1 == "sparrow" ]]; then
+  $SMAKE_BIN --init
+  $SMAKE_BIN $MAKE_PARAMS
+  mv sparrow/src/jtag/*.i $SMAKE_OUT
+elif [[ $1 == "infer" ]]; then
+  $INFER_BIN capture -- make $MAKE_PARAMS
+  mv infer-out $OUT
+else
+  echo "Unknown build target"
+  exit 1
+fi

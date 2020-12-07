485b5108a13bef8135cbe905bd3c50ee8e97d4ab
hyunsukimsokcho <hskimsokcho@gmail.com>
2020-12-07 16:53:39 +0900

    [Bin] add run-sparrow

---
+++ bin/run-sparrow.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Usage: ./run-sparrow.sh CONTAINER_NAME [ SPARROW_ARGS .. ]
+
+DOCKER_IMAGE=$1
+
+SPARROW_ARGS=${@:2}
+
+CONTAINER_IMAGE=$(docker run -i --detach $DOCKER_IMAGE)
+
+BUILD=/src/build.sh
+OUT=/out/smake-out
+
+docker exec $CONTAINER_IMAGE $BUILD sparrow
+
+docker cp $CONTAINER_IMAGE:$OUT .
+
+docker kill $CONTAINER_IMAGE
+
+sparrow $SPARROW_ARGS smake-out/*.i

e3085190f212610fa0ee1ba0ee10df7e0babf1a7
hyunsukimsokcho <hskimsokcho@gmail.com>
2020-12-08 16:21:13 +0900

    [Benchmark] add wget-1.12

---
+++ benchmark/wget/1.12/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+./autogen.sh
+./configure
+
+MAKE_PARAMS="install -j"
+SMAKE_I_DIR="sparrow/src/wget"
+
+if [[ $1 == "sparrow" ]]; then
+  $SMAKE_BIN --init
+  $SMAKE_BIN $MAKE_PARAMS
+  cp $SMAKE_I_DIR/*.i $SMAKE_OUT
+elif [[ $1 == "infer" ]]; then
+  $INFER_BIN capture -- $MAKE_PARAMS
+  mv infer-out $OUT
+else
+  echo "Unknown build target"
+  exit 1
+fi

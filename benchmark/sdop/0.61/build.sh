1a6a89e6197c5a7c9b67ff9a9724595046e6e5b5
hyunsukimsokcho <hskimsokcho@gmail.com>
2020-12-09 16:21:08 +0900

    [Benchmark] add sdop

---
+++ benchmark/sdop/0.61/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+./configure
+
+MAKE_PARAMS="-j"
+SMAKE_I_DIR="sparrow/src/sdop"
+
+if [[ $1 == "sparrow" ]]; then
+  $SMAKE_BIN --init
+  $SMAKE_BIN $MAKE_PARAMS
+  cp $SMAKE_I_DIR/*.i $SMAKE_OUT
+elif [[ $1 == "infer" ]]; then
+  $INFER_BIN capture -- $MAKE_PARAMS
+  mv infer-out $OUT
+else
+  echo "Unknown build target"
+  exit 1
+fi

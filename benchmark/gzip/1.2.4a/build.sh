49d7277735d3971b4537eb6377b1e87993dc7c02
hyunsukimsokcho <hskimsokcho@gmail.com>
2020-12-09 13:02:11 +0900

    [Benchmark] fix build script

--- benchmark/gzip/1.2.4a/build.sh
+++ benchmark/gzip/1.2.4a/build.sh
@@ -7,6 +7,7 @@ SMAKE_I_DIR="sparrow"
 
 if [[ $1 == "sparrow" ]]; then
   $SMAKE_BIN --init
+  $SMAKE_BIN ./configure
   $SMAKE_BIN $MAKE_PARAMS
   cp $SMAKE_I_DIR/*.i $SMAKE_OUT
 elif [[ $1 == "infer" ]]; then

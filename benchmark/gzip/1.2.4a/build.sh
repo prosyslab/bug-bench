3e61a566736e2994df50e746c8b0d91de643cdd5
hyunsukimsokcho <hskimsokcho@gmail.com>
2020-12-14 10:34:30 +0900

    [Benchmark] run smake in gzip

--- benchmark/gzip/1.2.4a/build.sh
+++ benchmark/gzip/1.2.4a/build.sh
@@ -3,11 +3,10 @@
 ./configure
 
 MAKE_PARAMS="-j"
-SMAKE_I_DIR="sparrow"
+SMAKE_I_DIR="sparrow/gzip"
 
 if [[ $1 == "sparrow" ]]; then
   $SMAKE_BIN --init
-  $SMAKE_BIN ./configure
   $SMAKE_BIN $MAKE_PARAMS
   cp $SMAKE_I_DIR/*.i $SMAKE_OUT
 elif [[ $1 == "infer" ]]; then

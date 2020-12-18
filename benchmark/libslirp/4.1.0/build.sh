6ec398766e49e36dca86e7586f6f83009776aac5
hyunsukimsokcho <hskimsokcho@gmail.com>
2020-12-18 15:15:17 +0900

    [Benchmark] add libslrip-4.1.0

---
+++ benchmark/libslirp/4.1.0/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+
+MAKE_PARAMS="-j"
+SMAKE_I_DIR="TODO"
+
+if [[ $1 == "sparrow" ]]; then
+  $SMAKE_BIN --init
+  $SMAKE_BIN $MAKE_PARAMS
+  cp $SMAKE_I_DIR/*.i $SMAKE_OUT
+elif [[ $1 == "infer" ]]; then
+  $INFER_BIN capture -- make $MAKE_PARAMS
+  cp -r infer-out $OUT
+else
+  echo "Unknown build target"
+  exit 1
+fi

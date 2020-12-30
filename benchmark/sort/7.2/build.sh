cfa18b3a26295df8b002fb9b7c72a5fbb0c16539
hyunsukimsokcho <hskimsokcho@gmail.com>
2020-12-30 16:51:04 +0900

    [Benchmark] fix target to move after smake

--- benchmark/sort/7.2/build.sh
+++ benchmark/sort/7.2/build.sh
@@ -3,13 +3,13 @@
 ./configure
 
 MAKE_PARAMS="-j"
-SMAKE_I_DIR="sparrow/src"
+SMAKE_I_DIR="sparrow/src/sort"
 
 if [[ $1 == "sparrow" ]]; then
   $SMAKE_BIN --init
   $SMAKE_BIN ./configure
   $SMAKE_BIN $MAKE_PARAMS
-  cp $SMAKE_I_DIR/sort.o.i $SMAKE_OUT
+  cp $SMAKE_I_DIR/*.i $SMAKE_OUT
 elif [[ $1 == "infer" ]]; then
   $INFER_BIN capture -- $MAKE_PARAMS
   mv infer-out $OUT

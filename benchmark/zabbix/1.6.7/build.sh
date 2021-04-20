13a48faa3b48d2f0686669047e46db4d6cafcee1
Kihong Heo <kihong.heo@kaist.ac.kr>
2021-04-20 12:07:02 +0900

    [Benchmark] fix zabbix

--- benchmark/zabbix/1.6.7/build.sh
+++ benchmark/zabbix/1.6.7/build.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
-./configure
+MAKE_PARAMS="-j"
+
+./configure --enable-server --with-mysql
 
 if [[ $1 == "sparrow" ]]; then
-  echo "TODO: $1"
-  exit 1
+  $SMAKE_BIN --init
+  $SMAKE_BIN $MAKE_PARAMS
+  cp sparrow/src/zabbix_server/zabbix_server/*.i $SMAKE_OUT
 elif [[ $1 == "infer" ]]; then
-  $INFER_BIN capture -- make
+  $INFER_BIN capture -- make $MAKE_PARAMS
   cp -r infer-out $OUT
 elif [[ $1 == "codeql" ]]; then
-  $CODEQL_BIN database create --language=cpp --command="make" codeql-db
+  $CODEQL_BIN database create --language=cpp --command="make $MAKE_PARAMS" codeql-db
   cp -r codeql-db $OUT
 else
   echo "Unknown build target"

a42d6cd8c81e1b467d82ad4b54c0d0b03bb38848
hyunsukimsokcho <hskimsokcho@gmail.com>
2021-04-21 15:27:03 +0900

    [Benchmark] fix label

--- benchmark/jasper/1.900.12/build.sh
+++ benchmark/jasper/1.900.12/build.sh
@@ -8,7 +8,7 @@ MAKE_PARAMS="-j"
 if [[ $1 == "sparrow" ]]; then
   $SMAKE_BIN --init
   $SMAKE_BIN $MAKE_PARAMS
-  mv sparrow/src/libxcursor/*.i $SMAKE_OUT
+  mv sparrow/src/appl/*.i $SMAKE_OUT
 elif [[ $1 == "infer" ]]; then
   $INFER_BIN capture -- make $MAKE_PARAMS
   cp -r infer-out $OUT

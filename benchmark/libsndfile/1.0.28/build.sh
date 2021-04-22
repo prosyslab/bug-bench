db7f70c9e8208a1730428fe447617ebee238b942
hyunsukimsokcho <hskimsokcho@gmail.com>
2021-04-22 14:59:50 +0900

    [Benchmark] add libsndfile-1.0.28

---
+++ benchmark/libsndfile/1.0.28/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+./autogen.sh
+./configure
+
+if [[ $1 == "sparrow" ]]; then
+  $SMAKE_BIN --init
+  $SMAKE_BIN -j
+  cp sparrow/src/*.i $SMAKE_OUT
+elif [[ $1 == "infer" ]]; then
+  $INFER_BIN capture -- make -j
+  mv infer-out $OUT
+else
+  echo "Unknown build target"
+  exit 1
+fi

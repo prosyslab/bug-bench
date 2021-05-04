37b5ab1147fdf5ed41214dcedccd875f89fb6851
kangwoosukeq <kangwoosuk1@gmail.com>
2021-05-04 14:00:09 +0900

    [Benchmark] fix build script for schismtracker

--- benchmark/schismtracker/20190722/build.sh
+++ benchmark/schismtracker/20190722/build.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
 autoreconf -i
-
-mkdir -p build && cd build && ../configure
+./configure
 
 if [[ $1 == "sparrow" ]]; then
   $SMAKE_BIN --init
   $SMAKE_BIN -j
   cp sparrow/schismtracker/*.i $SMAKE_OUT
 elif [[ $1 == "infer" ]]; then
-  $INFER_BIN capture -- make $MAKE_PARAMS
+  $INFER_BIN capture -- make
   cp -r infer-out $OUT
 else
   echo "Unknown build target"

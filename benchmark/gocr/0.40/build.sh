6028ef5d0dc10d72e1ee5ce04eb4b25cda82d548
kangwoosukeq <kangwoosuk1@gmail.com>
2021-06-30 10:38:50 +0900

    [Benchmark] add gocr-0.40

---
+++ benchmark/gocr/0.40/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+./configure
+
+if [[ $1 == "sparrow" ]]; then
+  $SMAKE_BIN --init
+  $SMAKE_BIN $MAKE_PARAMS
+  mv sparrow/src/optipng/*.i $SMAKE_OUT
+elif [[ $1 == "infer" ]]; then
+  $INFER_BIN capture -- make
+  cp -r infer-out $OUT
+elif [[ $1 == "codeql" ]]; then
+  $CODEQL_BIN database create --language=cpp --command="make $MAKE_PARAMS" codeql-db
+  cp -r codeql-db $OUT
+else
+  echo "Unknown build target"
+  exit 1
+fi

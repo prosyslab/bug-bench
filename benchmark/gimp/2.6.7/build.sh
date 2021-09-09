b0cfec8ee73e25ba236178a19e6e037bf05a6834
hyunsukimsokcho <hskimsokcho@gmail.com>
2021-09-09 18:05:37 +0900

    [Benchmark] fix gimp-2.6.7

--- benchmark/gimp/2.6.7/build.sh
+++ benchmark/gimp/2.6.7/build.sh
@@ -41,7 +41,7 @@ MAKE_PARAMS="-j"
 if [[ $1 == "sparrow" ]]; then
   $SMAKE_BIN --init
   $SMAKE_BIN $MAKE_PARAMS
-  mv sparrow/src/libxcursor/*.i $SMAKE_OUT
+  mv sparrow/*.i $SMAKE_OUT
 elif [[ $1 == "infer" ]]; then
   $INFER_BIN capture -- make $MAKE_PARAMS
   cp -r infer-out $OUT

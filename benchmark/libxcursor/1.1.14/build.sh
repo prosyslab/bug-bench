84bef572b27b349411a4728bf9cbfc1c6deabe4e
seungwan.kwon <seungwan.kwon@prosys.kr>
2021-10-05 01:33:36 +0000

    [Benchmark] fix the build bug of libxcursor-1.1.14

--- benchmark/libxcursor/1.1.14/build.sh
+++ benchmark/libxcursor/1.1.14/build.sh
@@ -7,7 +7,7 @@ MAKE_PARAMS="-j"
 if [[ $1 == "sparrow" ]]; then
   $SMAKE_BIN --init
   $SMAKE_BIN $MAKE_PARAMS
-  mv sparrow/src/libxcursor/*.i $SMAKE_OUT
+  mv sparrow/src/*.i $SMAKE_OUT
 elif [[ $1 == "infer" ]]; then
   $INFER_BIN capture -- make $MAKE_PARAMS
   cp -r infer-out $OUT

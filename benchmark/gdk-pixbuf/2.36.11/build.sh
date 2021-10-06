e759743c493fc0d8f314a5b91a05ce9aed8bef39
seungwan.kwon <seungwan.kwon@prosys.kr>
2021-10-06 06:29:33 +0000

    [Benchmark] fix the build bug of gdk-pixbuf-2.36.11

--- benchmark/gdk-pixbuf/2.36.11/build.sh
+++ benchmark/gdk-pixbuf/2.36.11/build.sh
@@ -3,8 +3,10 @@
 ./autogen.sh
 
 if [[ $1 == "sparrow" ]]; then
-  echo "Sparrow not supported"
-  exit 1
+  $SMAKE_BIN --init
+  $SMAKE_BIN ./configure
+  $SMAKE_BIN -j
+  mv sparrow/gdk-pixbuf/.libs/*.i $SMAKE_OUT
 elif [[ $1 == "infer" ]]; then
   $INFER_BIN capture -- make $MAKE_PARAMS
   cp -r infer-out $OUT

6bcd4a00166b2084869dc05ca05aaee078fdc24f
hyunsukimsokcho <hskimsokcho@gmail.com>
2022-05-25 07:08:01 +0900

    [Benchmark] add openjpeg

---
+++ benchmark/openjpeg/c02f14/build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y autoconf libtool-bin
+
+./bootstrap.sh
+./configure
+
+if [[ $1 == "sparrow" ]]; then
+  echo "TODO: $1"
+  exit 1
+elif [[ $1 == "infer" ]]; then
+  echo "TODO: $1"
+  exit 1
+elif [[ $1 == "codeql" ]]; then
+  echo "TODO: $1"
+  exit 1
+else
+  make -j
+fi

3355585c152f27f622da7aa1746be744372a13fc
Hhro <seungwan.kwon@prosys.kr>
2023-03-10 17:23:04 +0900

    [benchmark] fix the build bugs of libslirp and urjtag

--- benchmark/libslirp/4.1.0/build.sh
+++ benchmark/libslirp/4.1.0/build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 MAKE_PARAMS="-j"
 SMAKE_I_DIR="sparrow/libslirp.a"
 
@@ -11,6 +10,16 @@ if [[ $1 == "sparrow" ]]; then
 elif [[ $1 == "infer" ]]; then
   $INFER_BIN capture -- make $MAKE_PARAMS
   cp -r infer-out $OUT
+elif [[ $1 == "haechi" ]]; then
+  export CC=$GCLANG_BIN
+  export CFLAGS="-fno-discard-value-names -O0 -Xclang -disable-O0-optnone -g"
+  make $MAKE_PARAMS
+  for f in src/*; do
+    $GET_BC_BIN $f &&
+      llvm-dis -o $f.ll $f.bc &&
+      opt -mem2reg -S -o $f.ll $f.ll
+  done
+  llvm-link-13 -S src/*.ll -o $HAECHI_OUT/libslirp.ll
 else
   echo "Unknown build target"
   exit 1
--- benchmark/urjtag/0.8/build.sh
+++ benchmark/urjtag/0.8/build.sh
@@ -1,16 +1,27 @@
 #!/usr/bin/env bash
 
-./configure
-
 MAKE_PARAMS="-j"
 
 if [[ $1 == "sparrow" ]]; then
+  ./configure
   $SMAKE_BIN --init
   $SMAKE_BIN $MAKE_PARAMS
   mv sparrow/src/jtag/*.i $SMAKE_OUT
 elif [[ $1 == "infer" ]]; then
+  ./configure
   $INFER_BIN capture -- make $MAKE_PARAMS
   cp -r infer-out $OUT
+elif [[ $1 == "haechi" ]]; then
+  export CC=$GCLANG_BIN
+  export CFLAGS="-fno-discard-value-names -O0 -Xclang -disable-O0-optnone -g"
+  # avoid multiple definition error
+  sed -i '340s/.*/\/\/\0/' /src/urjtag/src/svf/svf_flex.c
+  ./configure
+  make $MAKE_PARAMS
+  EXT_TARGET=src/jtag
+  $GET_BC_BIN $EXT_TARGET &&
+  llvm-dis -o $EXT_TARGET.ll $EXT_TARGET.bc &&
+  opt -mem2reg -S -o $HAECHI_OUT/$(basename $EXT_TARGET).ll $EXT_TARGET.ll
 else
   echo "Unknown build target"
   exit 1

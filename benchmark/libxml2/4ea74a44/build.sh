d06bad5af38bc78ef4ee513607d1511ba4c6d418
zackcglee <leechanggong@gmail.com>
2023-07-25 23:01:50 +0900

    [bugbench] add rest of vulunfix benchmark

---
+++ benchmark/libxml2/4ea74a44/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+./autogen.sh
+
+MAKE_PARAMS="-j"
+
+if [[ $1 == "sparrow" ]]; then
+  $SMAKE_BIN --init
+  $SMAKE_BIN $MAKE_PARAMS
+  mv sparrow/*.i $SMAKE_OUT
+elif [[ $1 == "infer" ]]; then
+  $INFER_BIN compile -- cmake ..
+  $INFER_BIN capture -- make $MAKE_PARAMS
+  cp -r infer-out $OUT
+else
+  echo "Unknown build target"
+  exit 1
+fi

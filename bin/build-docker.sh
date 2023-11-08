0145d672fdacafd6a4c9c7b919a92693e19c48e8
Yeonhee Ryou <yeonhee1ryou@gmail.com>
2023-11-08 09:06:10 +0900

    [script] bin/build-docker.sh: add base version to image tag

--- bin/build-docker.sh
+++ bin/build-docker.sh
@@ -2,17 +2,23 @@
 
 set -e
 
+# Check the base ubuntu version of docker/Dockerfile
+BASE_VERSION="22.04"
+
 PROJECT_HOME="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd ../ && pwd)"
 
 build_base() {
   echo "build prosyslab/bug-bench-base"
   docker build --no-cache -t prosyslab/bug-bench-base $PROJECT_HOME/docker
   docker push prosyslab/bug-bench-base
+  docker tag prosyslab/bug-bench-base prosyslab/bug-bench-base:$BASE_VERSION
+  docker push prosyslab/bug-bench-base:$BASE_VERSION
 }
 
 build() {
   benchmark=$1
-  version=$(basename $benchmark)
+  program_version=$(basename $benchmark)
+  version=$program_version-$BASE_VERSION
   program=$(dirname $benchmark | xargs basename)
   echo "build prosyslab/bug-bench-$program:$version"
   docker build --no-cache -t prosyslab/bug-bench-$program:$version $benchmark

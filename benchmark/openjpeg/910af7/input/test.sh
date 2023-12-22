ab16b3644d3a11d6e5784241da9022e6ecf5ee36
zackcglee <leechanggong@gmail.com>
2023-12-22 17:03:42 +0900

    [openjpeg] remove untrackable versions

--- benchmark/openjpeg/910af7/input/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BIN=$SRC/$OPENJPEG_910af7/applications/codec/j2k_to_image
-HERE=$(dirname $0)
-
-case $1 in
-  n1) $BIN -i $HERE/sample.jp2 -o out.bmp;;
-esac
--- benchmark/openjpeg/c02f14/input/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-BIN=$SRC/$OPENJPEG_c02f14/applications/codec/j2k_to_image
-HERE=$(dirname $0)
-
-case $1 in
-  n1) $BIN -i $HERE/CVE-2016-8691.j2k -o out.bmp;;
-  n2) $BIN -i $HERE/CVE-2016-8692.j2k -o out.bmp;;
-esac

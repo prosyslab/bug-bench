ecf152fd4e8237b41a17a25cea4ff9528da0809d
goodtaeeun <goodtaeeun@kaist.ac.kr>
2023-12-27 15:07:10 +0900

    [benchmark] libtiff: remove useless version, update useful version

--- benchmark/libtiff/4.0.6/build.sh
+++ benchmark/libtiff/4.0.6/build.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 
-./configure
+MAKE_PARAMS="-j"
 
 if [[ $1 == "sparrow" ]]; then
-  echo "TODO: $1"
-  exit 1
-elif [[ $1 == "infer" ]]; then
-  echo "TODO: $1"
-  exit 1
-elif [[ $1 == "codeql" ]]; then
-  echo "TODO: $1"
-  exit 1
+  ./configure
+  $SMAKE_BIN --init
+  $SMAKE_BIN $MAKE_PARAMS
+  mv sparrow/tools/.libs/rgb2ycbcr/*.i $SMAKE_OUT
+elif [[ $1 == "haechi" ]]; then
+  export CC=$GCLANG_BIN
+  export CFLAGS="-fcommon -fno-discard-value-names -O0 -Xclang -disable-O0-optnone -g"
+  ./configure
+  
+  $SMAKE_BIN --init
+  $SMAKE_BIN $MAKE_PARAMS
+  mv sparrow/tools/.libs/rgb2ycbcr/*.i $SMAKE_OUT
+  
+  $GET_BC_BIN tools/rgb2ycbcr.o &&
+    llvm-dis -o libtiff.ll tools/rgb2ycbcr.o.bc &&
+    opt -mem2reg -S -o $HAECHI_OUT/libtiff-4.0.6.ll libtiff.ll
 else
-  make -j
+  echo "Unknown build target"
+  exit 1
 fi
--- benchmark/libtiff/4.0.7/input/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BIN=$SRC/$LIBTIFF_4_0_7/tools/tiffcp
-HERE=$(dirname $0)
-
-case $1 in
-  n1) $BIN -i $HERE/sample.tif temp;;
-esac

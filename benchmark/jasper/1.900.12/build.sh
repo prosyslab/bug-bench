ed046363f9eb4252e516acc16743dcf6241a5cea
Re-st <garypark@kaist.ac.kr>
2024-02-14 17:15:08 +0900

    [benchmark] jasper: link library files into ll file, smake with clang

--- benchmark/jasper/1.900.12/build.sh
+++ benchmark/jasper/1.900.12/build.sh
@@ -10,7 +10,9 @@ if [[ $1 == "sparrow" ]]; then
   ./configure
   $SMAKE_BIN --init
   $SMAKE_BIN $MAKE_PARAMS
-  mv sparrow/src/appl/*.i $SMAKE_OUT
+  for f in $(find /src/jasper/sparrow/src -name "*.i" -not -path '*/\.*'); do
+    mv $f $SMAKE_OUT/$(basename $f)
+  done
 elif [[ $1 == "infer" ]]; then
   autoreconf -i
   ./configure
@@ -21,15 +23,26 @@ elif [[ $1 == "haechi" ]]; then
   export CFLAGS="-fno-discard-value-names -O0 -Xclang -disable-O0-optnone -g"
   autoreconf -i
   ./configure
-  
+
   $SMAKE_BIN --init
   $SMAKE_BIN $MAKE_PARAMS
-  mv sparrow/src/appl/*.i $SMAKE_OUT
+  for f in $(find /src/jasper/sparrow/src -name "*.i" -not -path '*/\.*'); do
+    mv $f $SMAKE_OUT/$(basename $f)
+  done
 
   EXT_TARGET=src/appl/jasper.o
   $GET_BC_BIN $EXT_TARGET &&
-  llvm-dis -o $EXT_TARGET.ll $EXT_TARGET.bc &&
-  opt -mem2reg -S -o $HAECHI_OUT/$(basename $EXT_TARGET).ll $EXT_TARGET.ll
+    llvm-dis -o $EXT_TARGET.ll $EXT_TARGET.bc &&
+    opt -mem2reg -S -o $EXT_TARGET.ll $EXT_TARGET.ll
+
+  ## link other sources in lib/ to a single file
+  for f in $(find src/libjasper -type f -not -path '*/\.*'); do
+    $GET_BC_BIN $f &&
+      llvm-dis -o $f.ll $f.bc &&
+      opt -mem2reg -S -o src/$(basename $f).ll $f.ll
+  done
+  llvm-link-13 -S src/*.ll $EXT_TARGET.ll -o $HAECHI_OUT/jasper-1.900.12.ll
+
 else
   make -j
 fi

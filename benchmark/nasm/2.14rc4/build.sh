ef453338cda5b143c3e583fa8e42833815648c6c
leehahoon <leehahoon1001@gmail.com>
2024-06-25 11:55:32 +0900

    [Benchmark] fix nasm script

--- benchmark/nasm/2.14rc4/build.sh
+++ benchmark/nasm/2.14rc4/build.sh
@@ -2,7 +2,7 @@
 export CFLAGS="-Wno-error"
 
 MAKE_PARAMS="-j"
-SMAKE_I_DIR="sparrow"
+SMAKE_I_DIR="sparrow/nasm"
 BIN_PATH="nasm"
 
 if [[ $1 == "sparrow" ]]; then
@@ -14,6 +14,7 @@ if [[ $1 == "sparrow" ]]; then
   done
 elif [[ $1 == "haechi" ]]; then
   export CC=$GCLANG_BIN
+  export CFLAGS="$CFLAGS -fno-discard-value-names -O0 -Xclang -disable-O0-optnone -g"
   ./configure
 
   $SMAKE_BIN --init
@@ -22,8 +23,8 @@ elif [[ $1 == "haechi" ]]; then
     mv $f $SMAKE_OUT/$(basename $f)
   done
   $GET_BC_BIN $BIN_PATH &&
-  llvm-dis -o $BIN_PATH.ll $BIN_PATH.bc &&
-  opt -mem2reg -S -o $HAECHI_OUT/$(basename $BIN_PATH).ll $BIN_PATH.ll
+    llvm-dis -o $BIN_PATH.ll $BIN_PATH.bc &&
+    opt -mem2reg -S -o $HAECHI_OUT/$(basename $BIN_PATH).ll $BIN_PATH.ll
 else
   echo "Unknown build target"
   exit 1

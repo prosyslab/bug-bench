7992b5447fca599df8fce0e70f762bf59ed1721c
Re-st <garypark@kaist.ac.kr>
2024-09-26 13:53:47 +0900

    [Benchmark] update libsndfile build.sh for including essential functions

--- benchmark/libsndfile/1.0.28/build.sh
+++ benchmark/libsndfile/1.0.28/build.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+SMAKE_I_DIR="sparrow/programs/.libs/sndfile-convert"
+BIN_PATH="programs/.libs/sndfile-convert"
+
 if [[ $1 == "sparrow" ]]; then
   ./autogen.sh
   ./configure
   $SMAKE_BIN --init
   $SMAKE_BIN -j
-  cp sparrow/src/.libs/libsndfile.so.1.0.28/*.i $SMAKE_OUT
+  cp $SMAKE_I_DIR/*.i $SMAKE_OUT
 elif [[ $1 == "infer" ]]; then
   ./autogen.sh
   ./configure
@@ -19,11 +22,19 @@ elif [[ $1 == "haechi" ]]; then
   
   $SMAKE_BIN --init
   $SMAKE_BIN -j
-  cp sparrow/src/.libs/libsndfile.so.1.0.28/*.i $SMAKE_OUT
+  cp $SMAKE_I_DIR/*.i $SMAKE_OUT
 
-  $GET_BC_BIN src/.libs/libsndfile.so.1.0.28 &&
-    llvm-dis -o libsndfile.so.1.0.28.ll src/.libs/libsndfile.so.1.0.28.bc &&
-    opt -mem2reg -S -o $HAECHI_OUT/libsndfile-1.0.28.ll libsndfile.so.1.0.28.ll
+  $GET_BC_BIN $BIN_PATH &&
+    llvm-dis -o $BIN_PATH.ll $BIN_PATH.bc &&
+	opt -mem2reg -S -o $BIN_PATH.ll $BIN_PATH.ll
+
+  ## link other source in .libs/ to a single file
+  LIB_PATH="src/.libs/libsndfile.so.1.0.28"
+  $GET_BC_BIN $LIB_PATH &&
+    llvm-dis -o $LIB_PATH.ll $LIB_PATH.bc &&
+    opt -mem2reg -S -o $LIB_PATH.ll $LIB_PATH.ll
+
+  llvm-link-13 -S $LIB_PATH.ll $BIN_PATH.ll -o $HAECHI_OUT/$(basename $BIN_PATH).ll
 
 else
   echo "Unknown build target"

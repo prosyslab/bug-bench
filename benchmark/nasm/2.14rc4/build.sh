3cf0c58f03681af74d7399894ea37499c7e91c52
Re-st <geon.park00@kaist.ac.kr>
2025-04-15 10:53:37 +0900

    [Benchmark] update nasm build script
    * Fetch all .o.i files corresponding to .o files used in linking the nasm binary.

--- benchmark/nasm/2.14rc4/build.sh
+++ benchmark/nasm/2.14rc4/build.sh
@@ -2,14 +2,20 @@
 export CFLAGS="-Wno-error"
 
 MAKE_PARAMS="-j"
+SMAKE_I_DIRS="sparrow/asm sparrow/common sparrow/disasm sparrow/macros \
+sparrow/nasmlib sparrow/output sparrow/rdoff sparrow/stdlib sparrow/x86"
+
 BIN_PATH="nasm"
 
 if [[ $1 == "sparrow" ]]; then
   ./configure
   $SMAKE_BIN --init
   $SMAKE_BIN $MAKE_PARAMS
-  cp sparrow/asm/*.i $SMAKE_OUT
-  cp sparrow/nasmlib/*.i $SMAKE_OUT
+  for SMAKE_I_DIR in $SMAKE_I_DIRS; do
+    for f in $(find $SMAKE_I_DIR -maxdepth 1 -name "*.i" -not -path '*/\.*'); do
+      mv $f $SMAKE_OUT/$(basename $f)
+    done
+  done
 elif [[ $1 == "haechi" ]]; then
   export CC=$GCLANG_BIN
   export CFLAGS="$CFLAGS -fno-discard-value-names -O0 -Xclang -disable-O0-optnone -g"
@@ -17,8 +23,11 @@ elif [[ $1 == "haechi" ]]; then
 
   $SMAKE_BIN --init
   $SMAKE_BIN $MAKE_PARAMS
-  cp sparrow/asm/*.i $SMAKE_OUT
-  cp sparrow/nasmlib/*.i $SMAKE_OUT
+  for SMAKE_I_DIR in $SMAKE_I_DIRS; do
+    for f in $(find $SMAKE_I_DIR -maxdepth 1 -name "*.i" -not -path '*/\.*'); do
+      mv $f $SMAKE_OUT/$(basename $f)
+    done
+  done
   $GET_BC_BIN $BIN_PATH &&
     llvm-dis -o $BIN_PATH.ll $BIN_PATH.bc &&
     opt -mem2reg -S -o $HAECHI_OUT/$(basename $BIN_PATH).ll $BIN_PATH.ll
